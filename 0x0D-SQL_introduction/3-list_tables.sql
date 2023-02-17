963413bb9c84d9c975e41ee3dd3bfbac2dd86a6f
Salawu O.J <sojbaba98@gmail.com>
2023-02-17 09:28:52 -0800

    database

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,2 @@
 -- This script lists all the tables in msql database
-dbname="$1"
--- Build the MySQL command to list tables
-mysql_cmd="USE ${dbname}; SHOW TABLES;"
--- Run the MySQL command and display the results
-mysql -e "${mysql_cmd}"
+SHOW TABLES;
