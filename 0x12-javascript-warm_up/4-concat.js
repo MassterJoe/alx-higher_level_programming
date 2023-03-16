df82a337354f4fbe5c51183cc11312e9687091ec
Salawu O Joseph <sojbaba98@gmail.com>
2023-03-16 14:15:21 -0700

    javascript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,8 @@ const process = require('process');
 const argv = process.argv;
 if (argv.length > 2) {
   console.log(argv[2] + ' ' + 'is' + ' ' + argv[3]);
+} else if (argv.length === 2) {
+  console.log(argv[2] + ' ' + 'is' + ' ' + 'undefined');
+} else {
+  console.log('undefined' + ' ' + 'is' + ' ' + 'undefined');
 }
