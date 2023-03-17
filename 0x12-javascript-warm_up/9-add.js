5c0620ac663c5d17730c4bd3b2d03c4d7f2a5e97
Salawu O Joseph <sojbaba98@gmail.com>
2023-03-17 15:37:54 -0700

    javascript

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+const argv = process.argv;
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
+add(a, b);
