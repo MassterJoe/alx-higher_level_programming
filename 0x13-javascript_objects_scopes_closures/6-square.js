79f3c82ec37dae232ff5ceec615b41b03e6b3495
Salawu O Joseph <sojbaba98@gmail.com>
2023-03-18 13:39:46 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const SquareK = require('./5-square.js');
 class Square extends SquareK {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      let row = "";
+      let row = '';
       for (let j = 0; this.width; j++) {
         row += 'C';
       }
-	console.log(row);
+      console.log(row);
     }
   }
 }
