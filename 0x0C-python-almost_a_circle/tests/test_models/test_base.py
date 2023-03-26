175b6baca639cfa734f1e26dff177072e52671b0
Salawu O Joseph <sojbaba98@gmail.com>
2023-03-26 12:28:53 -0400

    new

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines unittests for base.py
+unittest classes
+    TestBase_instantiation
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestBase_instantiation(unittest.TestCase):
+    """instantiation of the base class"""
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+
+    def test_None_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
