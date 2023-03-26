c2aa0c84eb90b94200ff3dfb2e6e12ac3859aad4
Salawu O Joseph <sojbaba98@gmail.com>
2023-03-26 11:30:15 -0400

    new

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""Rectangle class inheriting from base class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ Represents a rectangle """
+    """Represents a rectangle """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
