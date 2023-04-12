3294a79bc2e8acafecab4dd39d09cb92be909892
Salawu O Joseph <sojbaba98@gmail.com>
2023-04-12 16:42:32 -0400

    mes

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url).text
     if r.status_code >= 400:
-        print("Error code: {}".format(e.code))
+        print("Error code: {}".format(r.status_code))
     else:
         print(r)
