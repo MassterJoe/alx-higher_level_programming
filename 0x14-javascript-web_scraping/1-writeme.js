6dec8cea6a60c69556a1c617b20fd52bcdbec14f
Salawu O Joseph <104222651+MassterJoe@users.noreply.github.com>
2023-04-25 22:50:58 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', err => {
+fs.writeFile(process.argv[2], process.argv[3], err => {
   if (err) {
     console.error(err);
     return;
