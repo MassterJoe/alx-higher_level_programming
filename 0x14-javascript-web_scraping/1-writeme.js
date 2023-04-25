3acf6570f537419a52b1582615fb7e7bd68ac66b
Salawu O Joseph <104222651+MassterJoe@users.noreply.github.com>
2023-04-25 22:55:21 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], err => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log('File written successfully!');
 });
